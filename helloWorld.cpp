53ec4ff44e1f487c6f8d3fbb4cb1d509a3066b97
eric schles <ericschles@gmail.com>
2012-09-01 00:45:31 -0400

    hello

---
+++ helloWorld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string name;
+    int age;
+
+    cout<<"Hello world";
+    cout<<"What's your name?\n";
+    cin>>name;
+    cout<<endl<<"How old are you?";
+    cin>>age;
+    cout<<endl;
+    cout<<"So you're name is "<<name
+    <<" and you are "<<age<<" years old";
+
+    return 0;
+
+}
